301d263e6e148afad428b31abaee3faa636f9a0f
julio cesar rodriguez nieves <jcrodriguez@alioit.com>
2019-04-02 16:29:30 -0700

    added describe

--- src/sagas.test.js
+++ src/sagas.test.js
@@ -2,12 +2,18 @@ import { call, put } from 'redux-saga/effects';
 
 import { incrementAsync, delay } from './sagas';
 
-it('incrementAsync Saga test', () => {
+describe('incrementAsync Saga test', () => {
   const gen = incrementAsync();
 
-  expect(gen.next().value).toEqual(call(delay, 1000));
+  it('delay', () => {
+    expect(gen.next().value).toEqual(call(delay, 1000));
+  });
 
-  expect(gen.next().value).toEqual(put({ type: 'INCREMENT' }));
+  it('INCREMENT', () => {
+    expect(gen.next().value).toEqual(put({ type: 'INCREMENT' }));
+  });
 
-  expect(gen.next()).toEqual({ done: true, value: undefined });
+  it('done', () => {
+    expect(gen.next()).toEqual({ done: true, value: undefined });
+  });
 });
