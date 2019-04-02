707d583b5848194b73e96fad9f1c9b0636e3b194
DiaDeMuertos <imworkingnow@msn.com>
2019-04-01 22:11:03 -0700

    added base

---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import action from './action';
+
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) => {
+  console.log('mmm');
+  return (
+    <div>
+      <button onClick={onIncrementAsync}>Increment after 1 second</button>{' '}
+      <button onClick={onIncrement}>Increment</button>{' '}
+      <button onClick={onDecrement}>Decrement</button>
+      <hr />
+      <div>Clicked: {value} times</div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  value: state,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onIncrement: () => action('INCREMENT'),
+  onDecrement: () => action('DECREMENT'),
+  onIncrementAsync: () => action('INCREMENT_ASYNC'),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Counter);
