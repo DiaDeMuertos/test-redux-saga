a893fe0f626664d63cb594d7a9a1cb5863a42ca8
julio cesar rodriguez nieves <jcrodriguez@alioit.com>
2019-04-04 11:42:12 -0700

    added create reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,32 @@
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'INCREMENT_IF_ODD':
-      return state % 2 !== 0 ? state + 1 : state;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
+import { createReducer, Types as ReduxSauceTypes } from 'reduxsauce';
+import Immutable from 'seamless-immutable';
+import Types from './actionTypes';
+
+console.log(Types);
+
+const INITIAL_STATE = Immutable(0);
+
+export const increment = (state = INITIAL_STATE, action) => {
+  return state + 1;
 };
 
-export default counter;
+export const incrementIfOdd = (state = INITIAL_STATE, action) => {
+  return state + 1;
+};
+
+export const decrement = (state = INITIAL_STATE, action) => {
+  return state - 1;
+};
+
+export const defaultHandler = (state = INITIAL_STATE, action) => {
+  return state % 2 !== 0 ? state + 1 : state;
+};
+
+export const HANDLERS = {
+  [Types.INCREMENT]: increment,
+  [Types.INCREMENT_IF_ODD]: incrementIfOdd,
+  [Types.DECREMENT]: decrement,
+  [ReduxSauceTypes.DEFAULT]: defaultHandler,
+};
+
+export default createReducer(INITIAL_STATE, HANDLERS);
