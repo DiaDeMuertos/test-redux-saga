bf63095173fd54986b58893ab7e4c0d16af91261
DiaDeMuertos <imworkingnow@msn.com>
2019-06-30 13:28:49 -0700

    added counter state

--- src/Counter.js
+++ src/Counter.js
@@ -3,26 +3,26 @@ import { connect } from 'react-redux';
 import action from './action';
 import Creators from './actionCreator';
 
-const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) => {
+const Counter = ({ counter, onIncrement, onDecrement, onIncrementAsync }) => {
   return (
     <div>
       <button onClick={onIncrementAsync}>Increment after 1 second</button>{' '}
       <button onClick={onIncrement}>Increment</button>{' '}
       <button onClick={onDecrement}>Decrement</button>
       <hr />
-      <div>Clicked: {value} times</div>
+      <div>Clicked: {counter.value} times</div>
     </div>
   );
 };
 
 const mapStateToProps = state => ({
-  value: state,
+  counter: state.counter,
 });
 
 const mapDispatchToProps = () => ({
   onIncrement: () => action(Creators.increment()),
   onDecrement: () => action(Creators.decrement()),
-  onIncrementAsync: () => action(Creators.incrementAsync()),
+  onIncrementAsync: () => action(Creators.incrementAsync({ data: 'OK!' })),
 });
 
 export default connect(
